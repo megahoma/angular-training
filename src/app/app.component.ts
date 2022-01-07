74f1fa8cc494caaf8bd54c229c0f930d79abe59a
megahoma <timkrut5348@gmail.com>
2022-01-07 19:00:17 +0300

    initial counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  counter: number = 0;
+
+  get cannotDecrease(): boolean {
+    return this.counter <= 0;
+  }
+
+  increase(): void {
+    this.counter++;
+  }
+  decrease(): void {
+    this.counter--;
+  }
+}
