12dda0f83faf5ca1cd419a076e001b7db52eaa35
megahoma <timkrut5348@gmail.com>
2022-01-09 00:58:35 +0300

    added tasks and task item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, ButtonComponent],
+  declarations: [AppComponent, HeaderComponent, ButtonComponent, TasksComponent, TaskItemComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
